c13d7517aaffca9939433c4351393ced9dadd102
liamo3323 <liam3323@gmail.com>
2024-02-15 22:01:34 +0000

    finished first assignment

--- 1-scaled_dot_product_attetion.py
+++ 1-scaled_dot_product_attetion.py
@@ -26,19 +26,28 @@ def scaled_dot_product(q, k, v, mask=None):
 
 if __name__ == '__main__':
     # Test case
-    seq_len, d_k = 3, 2
-    torch.manual_seed(3025)
-    q = torch.randn(seq_len, d_k)
-    k = torch.randn(seq_len, d_k)
-    v = torch.randn(seq_len, d_k)
-    valid = torch.tensor([[-1.0142, -1.9154],
-            [-0.4535, -1.6679],
-            [ 0.5474, -1.2476]])
+    # seq_len, d_k = 3, 2
+    # torch.manual_seed(3025)
+    # q = torch.randn(seq_len, d_k)
+    # k = torch.randn(seq_len, d_k)
+    # v = torch.randn(seq_len, d_k)
+    # valid = torch.tensor([[-1.0142, -1.9154],
+    #         [-0.4535, -1.6679],
+    #         [ 0.5474, -1.2476]])
+    # output, attention_weight = scaled_dot_product(q,k,v)
+    # differences = (output - valid).mean()
+    # print(q)
+    # print(k)
+    # print(v)
+    # print(output)
+    # print(differences)
+    # assert torch.abs(differences) < 0.0001, 'the product must be similar output as expected'
+
+    torch.set_printoptions(precision=16)
+    q = torch.tensor([[-1.0590970516204834, 0.7565467357635498, 0.20126891136169434, -0.6364921927452087], [0.004584392067044973, -2.307931900024414, 1.4653981924057007, 0.8097707629203796], [0.4444141685962677, -1.1070520877838135, -0.4160226285457611, 1.122387170791626]])
+    k = torch.tensor([[-13.375662803649902, 8.191596031188965, -32.305240631103516, 44.86949157714844], [15.18038272857666, -61.08460998535156, -41.45825958251953, -32.916629791259766], [48.51971435546875, 32.51784133911133, 1.7294328212738037, -62.1268310546875]])
+    v = torch.tensor([[-36.20771408081055, 26.2475643157959, 8.842000961303711, 43.27354431152344], [-62.30213165283203, 41.48006820678711, -6.880943775177002, -21.451093673706055], [-5.928592205047607, -0.6987577080726624, -12.249312400817871, 48.32103729248047]])
     output, attention_weight = scaled_dot_product(q,k,v)
-    differences = (output - valid).mean()
-    print(q)
-    print(k)
-    print(v)
-    print(output)
-    print(differences)
-    assert torch.abs(differences) < 0.0001, 'the product must be similar output as expected'
+
+    print(attention_weight)
+    # print(output)
--- 2-multi-head-attention.py
+++ 2-multi-head-attention.py
@@ -242,51 +242,47 @@ if __name__ == '__main__':
     num_classes   = train_loader.dataset.num_categories
     num_heads     = 4
     num_layers    = 10
-    dropout       = 0.0
-    input_dropout = 0.0
-
 
     model = TransformerPredictor(input_dim, model_dim, num_classes, num_heads, num_layers, dropout, input_dropout)
     loss_fn = nn.CrossEntropyLoss()
     # optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum = 0.9)
     optimizer = optim.Adam(self.parameters(), lr=self.hparams.lr)
 
-    def train_one_epoch(epoch_index, tb_writer):
-        running_loss = 0.
-        last_loss = 0.
+    def train_one_epoch(epoch_index):
+    running_loss = 0.
+    last_loss = 0.
 
-        # Here, we use enumerate(training_loader) instead of
-        # iter(training_loader) so that we can track the batch
-        # index anDd do some intra-epoch reporting
-        for i, data in enumerate(train_loader):
-            # Every data instance is an input + label pair
-            inputs, labels = data
-            # inputs = inputs.to(torch.float32)
-            inp_data = F.one_hot(inputs, num_classes=10).float()
+    # Here, we use enumerate(training_loader) instead of
+    # iter(training_loader) so that we can track the batch
+    # index and do some intra-epoch reporting
+    for i, data in enumerate(train_loader):
+        # Every data instance is an input + label pair
+        inputs, labels = data
+        inputs = F.one_hot(inputs, num_classes=10).float()
 
-            # Zero your gradients for every batch!
-            optimizer.zero_grad()
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
 
-            # Make predictions for this batch
-            outputs = model(inp_data)
+        # Make predictions for this batch
+        outputs = model(inputs)
 
-            # Compute the loss and its gradients
-            loss = loss_fn(outputs.view(-1,10), labels.view(-1))
-            loss.backward()
+        # Compute the loss and its gradients
+        loss = loss_func(outputs.view(-1,10), labels.view(-1))
+        
+        loss.backward()
 
-            # Adjust learning weights
-            optimizer.step()
+        # Adjust learning weights
+        optimizer.step()
 
-            # Gather data and report
-            running_loss += loss.item()
-            if i % 1000 == 999:
-                last_loss = running_loss / 1000 # loss per batch
-                print('  batch {} loss: {}'.format(i + 1, last_loss))
-                tb_x = epoch_index * len(train_loader) + i + 1
-                tb_writer.add_scalar('Loss/train', last_loss, tb_x)
-                running_loss = 0.
+        # Gather data and report
+        running_loss += loss.item()
+        if i % 1000 == 999:
+            last_loss = running_loss / 1000 # loss per batch
+            print('  batch {} loss: {}'.format(i + 1, last_loss))
+            tb_x = epoch_index * len(train_loader) + i + 1
+            running_loss = 0.
 
-        return last_loss
+    return last_loss
 
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     writer = SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
@@ -306,29 +302,5 @@ if __name__ == '__main__':
         # Set the model to evaluation mode, disabling dropout and using population
         # statistics for batch normalization.
         model.eval()
-
-        # Disable gradient computation and reduce memory consumption.
-        with torch.no_grad():
-            for i, vdata in enumerate(val_loader):
-                vinputs, vlabels = vdata
-                voutputs = model(vinputs)
-                vloss = loss_fn(voutputs, vlabels)
-                running_vloss += vloss
-
-        avg_vloss = running_vloss / (i + 1)
-        print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
-
-        # Log the running loss averaged per batch
-        # for both training and validation
-        writer.add_scalars('Training vs. Validation Loss',
-                        { 'Training' : avg_loss, 'Validation' : avg_vloss },
-                        epoch_number + 1)
-        writer.flush()
-
-        # Track best performance, and save the model's state
-        if avg_vloss < best_vloss:
-            best_vloss = avg_vloss
-            model_path = 'model_{}_{}'.format(timestamp, epoch_number)
-            torch.save(model.state_dict(), model_path)
 
         epoch_number += 1
